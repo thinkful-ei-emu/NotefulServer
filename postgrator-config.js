1b35530ddbf9433f2164341151f66483d52f8328
David Queen <dsqjr@msn.com>
2019-07-27 15:50:05 -0400

    updated postgrator.config

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 module.exports = {
   'migrationDirectory': 'migrations',
   'driver': 'pg',
+  'host': process.env.HOST,
+  'port': 5432,
   'username': process.env.DB_USER,
   'password': process.env.DB_PASS,
   'database': process.env.NODE_ENV === 'production' ? process.env.DB_NAME : process.env.DB_NAME_TEST
